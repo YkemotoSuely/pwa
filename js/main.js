064c95427953a27a79c7b2e576aeceb905ff5b00
Suely Ykemoto <suelyykemoto65@gmail.com>
2026-08-14 20:06:12 -0300

    Implementação dos eventos do service  worker

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ window.onload = () => {
     if('serviceWorker' in navigator){
         navigator.serviceWorker.register('./sw.js');
     }
-}
+} 
--- sw.js
+++ sw.js
@@ -1,10 +1,27 @@
 const cacheName = 'ola-pwa';
- 
-var filesToCache =[
+
+var filesToCache = [
     './',
     './manifest.webmanifest',
     './index.html',
     './css/style.css',
     './js/main.js'
-   
+
 ]
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(filesToCache)
+        )
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(Response => {
+                return response || fetch(event.request);
+            }
+        )
+    );
+});
